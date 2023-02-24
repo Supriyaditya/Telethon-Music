0021dc0328e8eb8a09cdfe75d489adc3b76fbd83
Supriyaditya <88608769+Supriyaditya@users.noreply.github.com>
2023-02-24 13:55:15 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6122404059:AAG7Mw9RtMSxl7TT3H3uU2Fj1K2bgz9Ywwc")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOGkBuxv5nutJBM-SkeB52igR6bGSOWdkL1fTrOlOkRGdTL064LBO6T4rLpS1RQFWQrtbNgJfh1JeW-Em1ERv0i9FE-hSlZrFcE_uKCBA1urYinwhGlRfN5zcck8QYvYTqWW7GDdmuysSMAmv8J3JfvwdRjMuWh_Sye9vj7vhnPT1NuG2BAL5hJ8nwKZQhKqgFjzWdnBIKCbYM_Ng7k_del-WF912pONbutM5xe02ve2zFdVZB03-Wv-kytb-33bN7bkclwubwUCwsdEgDojn1O0_VR3IqcxT-dloD0I-BgCPQvx5TGtY3TXWeGGtbgFkmOngvxaARgzEk4jpKdYD7lI4Sq4=")
+    API_ID = int(os.environ.get("APP_ID", "21242860"))
+    API_HASH = os.environ.get("API_HASH", "b1deaa84daa9d9f2b65725c21735e6c1")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6160772046:AAG7vY4SYZcD-wKDTggsgc7VvU5TKkE3B1Y")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOJYBuw7jYufog2oXPK8B0A91xKxR-dqULn6uzeP_oofwsT9b-zaUO_sFOOzcdu721YBLpLOgVErJDcg9W6uTFIK5ZoBs7JDsZTtDBW1k9E-MYgGL6JWrq6KLKFK5_NvwsTGXLQHJnrWUk77-ftyyrqu6d8oRLLZPIGkBmJOEH_k1Z2BcdoG7_S3oUjVlH8ocsdp9z6JApAdpvwnIsjhxsgNX6JDwwdYERjLo0re_V82DR85mZRZ5oRxM-DZIS3wut91s4pArIHZnrRzKGfHuJfHWEjd4L0848cPyFyymuA71oDG-ZwGrw-ftP5Kyfx9cRyF634IKekQc6PLElVMWRsdDaU0=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("@siddhantprobot", "")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
